f750a837d7d7adfd29b8a09f802aeee9fd186f04
Akinbi Tumininu <146536302+Akinbimary1209@users.noreply.github.com>
2024-12-20 00:07:09 +0100

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById('toggle-dark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function () {
+    this.classList.toggle('bi-brightness-low-fill');
+    if (this.classList.toggle('bi-moon')) {
+        body.style.background = 'white';
+        body.style.color = 'black'
+        body.style.transition = '1s'
+    }else{
+        body.style.background = 'black'
+        body.style.color = 'white'
+        body.style.transition = '1s'
+    }
+
+})
